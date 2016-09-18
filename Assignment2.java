e8272a63af69b3f0bc60a7533a8b641cc6457294
arpchauh <arpitchauhan10@gmail.com>
2016-09-18 11:29:55 +0530

    Add files via upload

---
+++ Assignment2.java
@@ -0,0 +1,25 @@
+package CoeJavaSession1;
+
+public class Assignment2 {
+	
+	public static void main(String args[]){
+		
+		int a=20;
+		int b=10;
+		
+		b=a--;
+		System.out.println("The value of integer b after increment is : "+b);
+		b=--a;
+		System.out.println("The value of integer b before increment is : "+b);
+		
+	int c=a--;
+	System.out.println("The value of integer c is : "+c);
+	
+	int d = a>>2;
+	System.out.println("The value of integer d is : "+d);
+	
+	int e = a&b;
+	System.out.println("The value of integer e is : "+e);
+	}
+
+}
